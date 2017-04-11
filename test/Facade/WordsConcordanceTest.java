3dc7c1d94a53b303bb959ae1bf6344956c4ddae9
Pajama Sammy <Pajama Sammy@192.168.1.39>
2017-04-11 09:34:58 +0200

    Removed standard headers and unused imports

--- test/Facade/WordsConcordanceTest.java
+++ test/Facade/WordsConcordanceTest.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Facade;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -17,31 +8,7 @@ import static org.junit.Assert.*;
  * @author Ricardo van Dijke
  */
 public class WordsConcordanceTest {
-    
-    public WordsConcordanceTest()
-    {
-    }
-    
-    @BeforeClass
-    public static void setUpClass()
-    {
-    }
-    
-    @AfterClass
-    public static void tearDownClass()
-    {
-    }
-    
-    @Before
-    public void setUp()
-    {
-    }
-    
-    @After
-    public void tearDown()
-    {
-    }
-
+      
     /**
      * Test of ConcordanceWords method, of class WordsConcordance.
      */
